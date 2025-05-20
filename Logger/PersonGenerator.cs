9402b26f3ad4b8a8a540ceec4e7dc06d18d77cf9
ltrlynick <literallynickk@gmail.com>
2025-05-20 14:22:07 +0400

    Person Generator Class

---
+++ Logger/PersonGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleLogProject
+{
+    public class PersonGenerator
+    {
+        private string[] firstNames = [
+            "John", "Maria", "James", "Lewis", "Peter"
+        ];
+        
+        private string[] lastNames = [
+            "Lee", "Smith", "Griffin", "Bond", "Hopps"
+        ];
+
+        public string GeneratePerson()
+        {
+            Random random = new Random();
+            return firstNames[random.Next(0, firstNames.Length)] + " " + lastNames[random.Next(0, lastNames.Length)];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,23 @@ namespace ConsoleLogProject{
     class Program{
         static void Main(string[] args){
             ConsoleLogger consoleLogger = new ConsoleLogger();
+            PersonGenerator personGenerator = new PersonGenerator();
+
+            string person1 = personGenerator.GeneratePerson();
+            string person2 = personGenerator.GeneratePerson();
+            string person3 = personGenerator.GeneratePerson();
+            string person4 = personGenerator.GeneratePerson();
+            string person5 = personGenerator.GeneratePerson();
+
+            consoleLogger.LogMessage(person1, LogType.TRACE);
+            consoleLogger.LogMessage(person2, LogType.TRACE);
+            consoleLogger.LogMessage(person3, LogType.TRACE);
+            consoleLogger.LogMessage(person4, LogType.TRACE);
+            consoleLogger.LogMessage(person5, LogType.TRACE);
+
             consoleLogger.LogMessage("");
             consoleLogger.LogMessage("This is a test!", LogType.INFO);
+
             try
             {
                 throw new Exception("Something went wrong :(");
