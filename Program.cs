544814123873c6705d21b6923c6449f29bf9cfaf
nikoloz-code23 <literallynickk@gmail.com>
2025-05-27 19:09:14 +0400

    Added one more person

--- Program.cs
+++ Program.cs
@@ -12,12 +12,14 @@ namespace ConsoleLogProject{
             string person3 = personGenerator.GeneratePerson();
             string person4 = personGenerator.GeneratePerson();
             string person5 = personGenerator.GeneratePerson();
+            string person6 = personGenerator.GeneratePerson();
 
             consoleLogger.LogMessage(person1, LogType.TRACE);
             consoleLogger.LogMessage(person2, LogType.TRACE);
             consoleLogger.LogMessage(person3, LogType.TRACE);
             consoleLogger.LogMessage(person4, LogType.TRACE);
             consoleLogger.LogMessage(person5, LogType.TRACE);
+            consoleLogger.LogMessage(person6, LogType.TRACE);
 
             consoleLogger.LogMessage("");
             consoleLogger.LogMessage("This is a test!", LogType.INFO);
